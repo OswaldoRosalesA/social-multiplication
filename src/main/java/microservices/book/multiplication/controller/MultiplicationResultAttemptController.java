96399990a9154d15aa8c566a0a6283dab41c5ff5
Oswaldo Rosales <oswaldo.rosales.a@gmail.com>
2020-03-02 09:29:48 -0600

    Feature: Chapter 5 - Load Balancing

--- src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -1,19 +1,27 @@
 package microservices.book.multiplication.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.multiplication.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
-@RequiredArgsConstructor
 @RequestMapping(path = "/results")
 public class MultiplicationResultAttemptController {
 
     private final MultiplicationService multiplicationService;
+    private final int serverPort;
+
+    public MultiplicationResultAttemptController(MultiplicationService multiplicationService, @Value("${server.port}") int serverPort) {
+        this.multiplicationService = multiplicationService;
+        this.serverPort = serverPort;
+    }
 
     @PostMapping
     public ResponseEntity<MultiplicationResultAttempt> postResult(@RequestBody MultiplicationResultAttempt multiplicationResultAttempt) {
@@ -34,6 +42,7 @@ public class MultiplicationResultAttemptController {
 
     @GetMapping(path = "/{resultId}")
     public ResponseEntity<MultiplicationResultAttempt> getResultById(@PathVariable("resultId") long resultId) {
+        log.info("Retrieving result {} from server @ {}", resultId, serverPort);
         MultiplicationResultAttempt resultAttempt = multiplicationService.getResultById(resultId);
         return ResponseEntity.ok(resultAttempt);
     }
